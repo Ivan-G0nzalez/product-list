913235562a9a39432a774c6d22dbe64a72a8da0a
Ivan_G0nzalez <iegdlt@gmail.com>
2024-09-18 22:02:08 -0500

    Category and Product Model was created

--- Back/product/models.py
+++ Back/product/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from account.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, unique=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='category_posts')
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='category_product')
+
+    def __str__(self) -> str:
+        return self.name
+    
