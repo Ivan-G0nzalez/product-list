a5a196375a30da198f8a68c9965b25984be4edf2
Ivan_G0nzalez <iegdlt@gmail.com>
2024-09-18 22:09:15 -0500

    Serializer for Category and Product created

---
+++ Back/product/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Category, Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'category', 'name', 'description', 'price', 'stock', 'created_at', 'updated_at', 'image', 'author']
+        read_only_fields = ['created_at', 'updated_at']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description', 'created_at', 'updated_at', 'author']
+        read_only_fields = ['created_at', 'updated_at']
